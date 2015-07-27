d8d3ecaf62040b791cc36e7ad2a5bf82b8f4622b
tykim <goddes4@gmail.com>
2015-07-27 11:23:46 +0900

    static root 지정

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,6 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
