9cafc5e9126c8288efcdb66bd6eb8f6c84869d2d
tykim <goddes4@gmail.com>
2015-07-27 11:42:34 +0900

    static root 지정

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,10 +103,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
