722abe4c0ceeefd0f7b2ec579e8576e7a5e15886
tykim <goddes4@gmail.com>
2015-07-27 17:19:45 +0900

    static 설정 테스트

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
